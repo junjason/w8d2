16db9f4825f937f3f3547912bd6afc59d53babc0
Allon Nam <allon.nam96@gmail.com>
2023-10-10 14:21:16 -0400

    twoSum

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
@@ -22,3 +22,21 @@ Array.prototype.uniq = function() {
 // const unique = arr.uniq();
 // console.log(arr);
 // console.log(unique);
+
+Array.prototype.twoSum = function() {
+    let result = [];
+    for(let i = 0; i < this.length-1; i++){
+        for(let j = i+1; j < this.length; j++){
+            if (this[i]+this[j] === 0){
+                result.push([i,j]);
+            }
+        }
+    }
+    return result;
+};
+
+// const arr = [-1, 0, 2, -2, 1];
+// const sums = arr.twoSum();
+// console.log(arr);
+// console.log(sums);
+
