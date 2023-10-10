d79376d2ddb3a274398123296121261505c94a57
Jason Jun <junjason@umich.edu>
2023-10-10 14:13:50 -0400

    done with uniq

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
@@ -1,1 +1,24 @@
 // Your code here
+
+Array.prototype.uniq = function() {
+    // return this;
+    // let unique = [];
+    // for(let i = 0; i < this.length; i++) {
+    //     if (!unique.includes(this[i])) {
+    //         unique.push(this[i]);
+    //     }
+    // }
+    // return unique;
+
+    const set = new Set(this);  // set = [1,2,3]
+    let unique = [];
+    for (const element of set) {    // element is equal to each element in set
+        unique.push(element)
+    }
+    return unique;
+};
+
+// const arr = [1,2,2,3,3,3];
+// const unique = arr.uniq();
+// console.log(arr);
+// console.log(unique);
