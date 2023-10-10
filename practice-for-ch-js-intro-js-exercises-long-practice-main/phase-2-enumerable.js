da5468d58e0a4e9f621142820ea1c056fd259b2e
Jason Jun <junjason@umich.edu>
2023-10-10 15:01:35 -0400

    myReduce

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-2-enumerable.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-2-enumerable.js
@@ -1,1 +1,31 @@
 // Your code here
+Array.prototype.myEach = function(callback) {
+    for(let i = 0; i < this.length; i++) {
+        callback(this[i]);
+    }
+};
+
+// let arr = [1,2,3,4];
+// function print(n) {
+//     console.log(n);
+// }
+// arr.myEach(print);
+
+Array.prototype.myMap = function(callback) {
+    let mapped = [];
+    for(let i = 0; i < this.length; i++) {
+        mapped.push(callback(this[i]));
+    }
+    return mapped;
+};
+
+// let arr = [1,2,3,4];
+// function double(i) {
+//     return i*i;
+// }
+// let doubled = arr.myMap(double);
+// console.log(doubled);
+
+Array.prototype.myReduce = function(callback, initialValue) {
+
+}
